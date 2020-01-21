2704d152f7297c8193de6db5fc54961483a3eb28
qqLyamin <qqgithub@gmail.com>
2020-01-21 14:52:16 +0300

    output

--- klasters/kmeans.cpp
+++ klasters/kmeans.cpp
@@ -9,6 +9,7 @@
 #include <functional>
 #include <windows.h>
 #include <sstream>
+#include <locale>
 
 //file
 #include <stdio.h>
@@ -23,14 +24,14 @@ int main()
 {
 	auto begin = std::chrono::steady_clock::now();
 	int numCPU = getCountOfThreads();
-	
+
 	std::ifstream ifs;
 	ifs.open("input.txt", std::ifstream::in);
-	
+
 	std::string first_row;
 	std::getline(ifs, first_row);
 	std::istringstream ist(first_row);
-	
+
 	size_t dim = 0;
 	float val;
 	while (ist >> val) {
@@ -41,7 +42,7 @@ int main()
 
 	std::istringstream iss(first_row);
 	iss >> first;
-		
+
 	std::vector<point> points;
 	points.push_back(first);
 	point p(n);
@@ -51,7 +52,7 @@ int main()
 	}
 	const uint32_t pointsCounter = points.size(); //		(pointsCounter <= 1 000 000)
 	const uint16_t K = 2; //number of centers  (K < 1000)
-	
+
 	std::vector<uint32_t> partOfPoints(numCPU);
 	for (auto& x : partOfPoints) {
 		x = pointsCounter / numCPU;
@@ -72,10 +73,10 @@ int main()
 	for (size_t i = 0; i < cluster_centers.size(); ++i) {
 		clusters.push_back(std::make_shared<Cluster>(n));
 	}
+	ifs.close();
 
 	int iterCounter = 0;
 	while (true) {
-		std::mutex mt;
 		std::vector<std::thread> pkThreads;
 		pkThreads.reserve(numCPU);
 
@@ -93,15 +94,16 @@ int main()
 					}
 					clusters[clusterIndex]->add(points[j]);
 				}
-			});
+				});
 		}
 		for (auto& t : pkThreads) t.join();
 
 		bool stop = true; //did we find the solution?
+		float epsilon = 0.001;
 		for (size_t i = 0; i < clusters.size(); ++i) {
 			point p = clusters[i]->getCenter();
-			clusters[i].reset();
-			stop &= (p == cluster_centers[i]); //is center changed?
+			clusters[i]->reset();
+			stop &= getDistance(p, cluster_centers[i]) < epsilon; //is center changed? (== analog)
 			cluster_centers[i] = p;
 		}
 
@@ -109,6 +111,16 @@ int main()
 
 		if (stop) {
 			break;
+		}
+	}
+
+	{
+		std::ofstream ofs;
+		ofs.open("output.txt");
+		ofs << "\n...............\n";
+		for (size_t i = 0; i < cluster_centers.size(); ++i) {
+			if (i % n == 0) ofs << "\n";
+			ofs << cluster_centers[i];
 		}
 	}
 
